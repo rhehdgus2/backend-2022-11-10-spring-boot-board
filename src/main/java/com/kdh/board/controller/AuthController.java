8cb219cf08b1193b70d8d7d8f4ef14c3f5b38da8
rhehdgus2 <kdgkdg0217@naver.com>
2023-01-06 12:46:12 +0900

    Dto

---
+++ src/main/java/com/kdh/board/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.kdh.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kdh.board.dto.auth.AuthPostDto;
+import com.kdh.board.dto.auth.LoginDto;
+import com.kdh.board.dto.response.ResponseDto;
+import com.kdh.board.service.AuthService;
+
+@RestController
+@RequestMapping("api/auth/")
+public class AuthController {
+	
+	// @Autowired : 해당하는 클래스 인스턴스를 자동으로 생성(주입) 해준다.
+	@Autowired AuthService authService;
+
+	@PostMapping("")
+	public ResponseDto<LoginDto> login(@RequestBody AuthPostDto requestBody) {
+//		LoginDto result = new LoginDto("JWT", 3600000);
+//		return ResponseDto.setSuccess("login success", result);
+		
+		return authService.login(requestBody);
+	}
+}
---
+++ src/main/java/com/kdh/board/service/AuthService.java
@@ -0,0 +1,19 @@
+package com.kdh.board.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kdh.board.dto.auth.AuthPostDto;
+import com.kdh.board.dto.auth.LoginDto;
+
+import com.kdh.board.dto.response.ResponseDto;
+
+// @Service : 해당 클래스가 Service 레이어 역할을 한다.
+@Service
+public class AuthService {
+	
+	public ResponseDto<LoginDto> login(AuthPostDto dto) {
+		LoginDto result = new LoginDto("JWT", 3600000);
+		return ResponseDto.setSuccess("", result);	
+	}
+
+}
