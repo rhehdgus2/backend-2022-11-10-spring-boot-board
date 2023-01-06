60cc0878546385d1aee8c5a5b2bf228dd1b8be8d
EliotDavid <107159240+EliotDavid@users.noreply.github.com>
2023-01-06 09:31:46 +0900

    first commit

---
+++ src/main/java/com/kdh/board/dto/HelloDto.java
@@ -0,0 +1,14 @@
+package com.kdh.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloDto {
+	private String name;
+	private int age;
+
+}
